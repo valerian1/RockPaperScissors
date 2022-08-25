293b79ca2e34a6977e8bdd4e96404d1b49251f6a
Valerian <valerianmars@gmail.com>
2022-08-25 21:04:06 +0200

    Finish first part of UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var overallScore = "Player ? : ? Cpu"
+    @State private var playerChoice = "Unknown"
+    @State private var computerChoice = "Unknown"
+    
+    var playerScore = 0
+    var computerScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text(overallScore)
+                .titleStyle()
+            
+            Spacer()
+            Text(computerChoice)
+                .titleStyle()
+            Spacer()
+            Text(playerChoice)
+                .titleStyle()
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Rock")
+                        .threeButtons()
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Paper")
+                        .threeButtons()
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Scissors")
+                        .threeButtons()
+                })
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ButtonStyle: ViewModifier {
+    @State var buttonColor = Color.blue
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.system(size: 18))
+            .padding(.horizontal, 18)
+            .padding(.vertical, 12)
+            .background(buttonColor)
+            .cornerRadius(10)
+    }
+}
+
+extension View {
+    func threeButtons() -> some View {
+        modifier(ButtonStyle(buttonColor: .black))
+    }
+}
+
+struct Title: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.black)
             .padding()
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
     }
 }
 
